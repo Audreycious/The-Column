92901c2a7d35be06f611d168dbb9076b3fad0fcc
Audreycious <porcupine4@gmail.com>
2019-09-18 20:27:37 -0500

    completed account-signup-wireframe

--- src/App.js
+++ src/App.js
@@ -8,7 +8,28 @@ function App() {
         The Column
       </header>
       <main>
-        <section className="Landing-page">
+        <section className='Account-signup-page'>
+          <div className='Main-container'>
+            <form action="#" className="Signup-form">
+              <fieldset>
+                <legend>Signup for The Column</legend>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" />
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" />
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" />
+              </fieldset>
+              <p>Disclaimer</p>
+              <button type="submit">Join</button>
+            </form>
+          </div>
+        </section>
+
+
+        {/* <section className="Landing-page">
           <div className='Sidebar-link-container'>
             <a href="#">Login</a>
             <a href="#">Signup</a>
@@ -16,7 +37,7 @@ function App() {
           <div className="Description-container">
             Website Description
           </div>
-        </section>
+        </section> */}
       </main>
     </div>
   );
