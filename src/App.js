1305d369308e384946a28c65f2556724e9fa238f
Audreycious <porcupine4@gmail.com>
2019-09-18 20:07:00 -0500

    completed website description

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        The Column
       </header>
+      <main>
+        <section className="Landing-page">
+          <div className='Sidebar-link-container'>
+            <a href="#">Login</a>
+            <a href="#">Signup</a>
+          </div>
+          <div className="Description-container">
+            Website Description
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
