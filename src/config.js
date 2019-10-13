38f09f5c998f78ab8624a9ebf85d8d05dca56344
Audreycious <porcupine4@gmail.com>
2019-10-12 23:35:26 -0500

    switched back to backend config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 
 export default {
-    API_ENDPOINT: `http://localhost:8000/`,
-    // `https://the-column-app.herokuapp.com/`,
+    API_ENDPOINT: `https://the-column-app.herokuapp.com/`,
     TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
